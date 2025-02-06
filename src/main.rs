6c51aa50968c606c81f9e416e89ce8a6bafcc8ab
parmeniong <par.graikos@icloud.com>
2025-02-06 13:02:02 +0200

    Add cli

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,27 @@
-fn main() {}
+use clap::Parser;
+use std::path::PathBuf;
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// The input file
+    file: Option<PathBuf>
+}
+
+fn main() {
+    let args = Args::parse();
+
+    if let Some(file) = args.file {
+        run_file(file);
+    } else {
+        repl();
+    }
+}
+
+fn run_file(_file: PathBuf) {
+    todo!()
+}
+
+fn repl() {
+    todo!()
+}
